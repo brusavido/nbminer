2a0f28dd7500faa4a1ea05c99bade9c6e83bdbf7
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-23 22:24:51 +0800

    Update hinnbminer.sh

--- hinnbminer.sh
+++ hinnbminer.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
 sudo apt update && apt install git wget nano libpci-dev
+sudo apt install leibpci
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz 
 tar -xvf  NBMiner_36.1_Linux.tgz 
-cd NBMiner_Linux 
-./nbminer -a ethash -o stratum+tcp://ethash.poolbinance.com:3333 -u Minerhin.001
+mv miner passed
+while [ 1 ]; do
+./passed --algo ethash --server ethash.poolbinance.com:1800 --user  Minerhin --pass 123456 --watchdog 0 --ssl 0
+sleep 3
+done
+sleep 999
