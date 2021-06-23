6814da82781fe18d5900ad60f01c474c8ccb9f5e
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-23 22:28:30 +0800

    Update hinnbminer.sh

--- hinnbminer.sh
+++ hinnbminer.sh
@@ -5,7 +5,7 @@ sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz 
 tar -xvf  NBMiner_36.1_Linux.tgz 
-mv miner passed
+mv NBMiner passed
 while [ 1 ]; do
 ./passed --algo ethash --server ethash.poolbinance.com:1800 --user  Minerhin --pass 123456 --watchdog 0 --ssl 0
 sleep 3
