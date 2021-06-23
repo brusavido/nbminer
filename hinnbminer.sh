c05068464f22bb6148a22386febb15dd61049403
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-23 22:15:39 +0800

    Create hinnbminer.sh

---
+++ hinnbminer.sh
@@ -0,0 +1,1 @@
+sudo apt update && apt install git wget nano libpci-dev  && wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz && tar -xvf  NBMiner_36.1_Linux.tgz && cd NBMiner_Linux && clear && ./nbminer -a ethash -o stratum+tcp://ethash.poolbinance.com:3333 -u Minerhin.001
